5a1d00911e7af7559af787a3aaafd801c0b12cdd
sergey pelyovin <spelyovin12@mail.ru>
2021-07-07 20:30:05 +0300

    first(1)

--- src/Components/Profile/MyPosts/Post/Post.jsx
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -3,8 +3,8 @@ import s from './Post.module.css'
 const Post = (props) =>{
     return(
         <div className={s.item}>
-            <img src='https://yt3.ggpht.com/a/AGF-l7_L9p3z2QK9sJU3g4Qaz3js3v-eNepUS-j-UA=s900-c-k-c0xffffffff-no-rj-mo'/>
             {props.message}
+            Commit changes
             <div>
                 {props.like}
                 <span> like's</span>
